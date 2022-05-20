e70fb114fb7228c5b70df46bcc2c02fca2df261e
aisyahzabidi <zabidiaisyah77@gmail.com>
2022-05-20 21:25:32 +0800

    commit

--- assocArray.php
+++ assocArray.php
@@ -1,24 +1,25 @@
 <html>
     <body>
-        $month = array ('January','February','March','April','May','June','July','August','September',
-        'October','November','December');
-        $monthDays = ('January'=>31,'February'=> 28,'March'=>31,'April'=>30,'May'=>31,'June=>30','July'=>31,'August'=>31,'September'=>30,
-        'October'=> 31,'November'=>30,'December'=>31);
-        {
-            echo "$month[$i] - $item<br>";
-            i++;
-        } echo "<br>";
-        $month = array ('January','February','March','April','May','June','July','August','September',
-        'October','November','December');
-        $monthDays = ('January'=>31,'February'=> 28,'March'=>31,'April'=>30,'May'=>31,'June=>30','July'=>31,'August'=>31,'September'=>30,
-        'October'=> 31,'November'=>30,'December'=>31);
+        <?php
+        $month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
+        $monthDays = array ('January'=>31,'February'=>30,'March'=>31,'Apri'=>30,'May'=>31,'June'=>30,'July'=>31,
+        'August'=>31,'September'=>30,'October'=>31,'November'=>30,'December'=>31);
         foreach ($monthDays as $item)
         {
-            if($item == 30)
+            echo "month [i] - $item <br>";
+            $i++;
+        } echo "<br>";
+        $month = array ('January','February','March','April','May','June','July','August','September','October','November','December');
+        $monthDays = array ('January'=>31,'February'=>30,'March'=>31,'Apri'=>30,'May'=>31,'June'=>30,'July'=>31,
+        'August'=>31,'September'=>30,'October'=>31,'November'=>30,'December'=>31);
+        foreach ($monthDays as $item)
+        {
+            if ($item == 30)
             {
-                echo "$month[$i] - $item <br>";
+                echo "month[i] - $item <br>";
             }
-        } i++;
+            $i++;
+        }
         ?>
-    </body>
-    </html>
+        </body>
+        </html>
